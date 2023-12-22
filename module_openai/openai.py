fbb0b09b1545e83d182423e13f4fb853114cbb44
Administrator <daniel.ad0791@gmail.com>
2023-12-22 00:19:29 +0000

    Added base for falcon and openai

---
+++ module_openai/openai.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+client = OpenAI()
+
+assistant = client.beta.assistants.create(
+  name="Data visualizer",
+  description="Take a deep breath and go step by step with your answers. You are a medical professional and "
+              "researcher in the field of medicine, nutrition, exercise and statistics.",
+  model="gpt-4-1106-preview",
+  tools=[{"type": "code_interpreter"}],
+  file_ids=[file.id]
+)
